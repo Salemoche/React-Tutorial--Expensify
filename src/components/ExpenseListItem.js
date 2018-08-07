94498ae1d7648175ab7c35a8c1d2b0a5229c32c6
salemoche <gabriel.bach@zhdk.ch>
2018-08-07 16:56:55 +0200

    changes to expenselistitem

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { removeExpense } from '../actions/expenses';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = (props) => (
   <li>
     <Link to= {`edit/${props.id}`} >{props.description} </Link>
-    <p>{props.amount}</p>
-    <p>{props.createdAt}</p>
+    <p>
+      {numeral(props.amount / 100 ).format('$0,0.00')}
+       - 
+      {moment(props.createdAt).format('DD MMMM YYYY')}
+    </p>
   </li>
 );
 
